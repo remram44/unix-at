c099e3a291d80a1059971d6518718ebee726a53c
Remi Rampin <remirampin@gmail.com>
2018-11-14 14:33:54 -0500

    Add functional test, talking to real at daemon

--- tests.py
+++ tests.py
@@ -1,9 +1,11 @@
 import datetime
+import os
+import time
 import unittest
 import unix_at
 
 
-class TestUnixAt(unittest.TestCase):
+class TestUnit(unittest.TestCase):
     def test_parse(self):
         job = unix_at.Job.parse(b'19\tTue Jun 26 11:32:00 2018 a remram')
         self.assertEqual(job.name, '19')
@@ -22,6 +24,38 @@ class TestUnixAt(unittest.TestCase):
             unix_at.convert_time(datetime.datetime(2018, 6, 26, 11, 48, 0)),
             '11:48 2018-06-26',
         )
+
+
+class TestFunctional(unittest.TestCase):
+    def test_real(self):
+        """Test by talking to the actual at daemon."""
+        self.assertEqual(unix_at.list_jobs(), [])
+        if os.path.exists('/tmp/job1'):
+            os.remove('/tmp/job1')
+        if os.path.exists('/tmp/job2'):
+            os.remove('/tmp/job2')
+        job1 = unix_at.submit_shell_job('echo one >/tmp/job1',
+                                       'now + 1 minutes')
+        jobs = unix_at.list_jobs()
+        self.assertEqual(len(jobs), 1)
+        self.assertEqual(jobs, [job1])
+        job2 = unix_at.submit_python_job(open, 'now + 2 minutes',
+                                         '/tmp/job2', 'w')
+        jobs = unix_at.list_jobs()
+        self.assertEqual(len(jobs), 2)
+        self.assertEqual(set(jobs), set([job1, job2]))
+        script = unix_at.get_script_for_job(job1.name)
+        # Some systems will wrap execution in `SHELL << 'end'\n...\nend`
+        expected = b'echo one >/tmp/job1'
+        self.assertTrue(script.splitlines()[-1].strip() == expected or
+                        script.splitlines()[-2].strip() == expected)
+        time.sleep(60)
+        self.assertEqual(unix_at.list_jobs(), [job2])
+        self.assertTrue(os.path.exists('/tmp/job1'))
+        self.assertFalse(os.path.exists('/tmp/job2'))
+        time.sleep(60)
+        self.assertEqual(unix_at.list_jobs(), [])
+        self.assertTrue(os.path.exists('/tmp/job2'))
 
 
 if __name__ == '__main__':
