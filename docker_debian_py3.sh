bd2ff9097e3f3a0e2e7515bd17c5d402044c691c
Remi Rampin <remi@rampin.org>
2023-04-27 11:59:16 -0400

    Use venv on modern systems

--- docker_debian_py3.sh
+++ docker_debian_py3.sh
@@ -3,8 +3,10 @@
 set -eux
 
 apt-get update
-apt-get install -yy at python3 python3-pip
+apt-get install -yy at python3 python3-pip python3-venv
 atd &
+python3 -m venv venv
+. venv/bin/activate
 cd /unix_at
 pip3 install .
 python3 tests.py
