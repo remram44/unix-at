fd0d28931033c74919fe8f85c59c12aee822a27d
Remi Rampin <remi@rampin.org>
2023-10-11 13:39:59 -0400

    Add test case for batch job (queue 'b')

--- tests.py
+++ tests.py
@@ -11,6 +11,9 @@ class TestUnit(unittest.TestCase):
         self.assertEqual(job.name, '19')
         self.assertEqual(job.time, datetime.datetime(2018, 6, 26, 11, 32, 0))
 
+        job = unix_at.Job.parse(b'19\tTue Jun 26 11:32:00 2018 b remram')
+        self.assertIsNone(job)
+
         job = unix_at.Job.parse(b'job 20 at Tue Jun 26 11:37:00 2018\n')
         self.assertEqual(job.name, '20')
         self.assertEqual(job.time, datetime.datetime(2018, 6, 26, 11, 37, 0))
